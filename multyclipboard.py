5777870c4da144123f0ae95cbdcea159e901f6c8
lovejoy777 <salovejoy777@gmail.com>
2022-01-31 23:44:38 +0000

    added commands for save, load and list

--- multyclipboard.py
+++ multyclipboard.py
@@ -2,5 +2,20 @@ import sys
 import clipboard
 import json
 
+print(sys.argv)
 
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    print(command)
 
+    if command == "save":
+        print("save")
+    elif command == "load":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("unknown command")
+
+else:
+    print("please pass exactly one command.")
